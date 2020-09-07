38f7863d4dd9d6e810d5c58c4efaaf027b8c9b34
Joshua McArthur <jcmcarth06@gmail.com>
2020-09-07 21:49:55 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -20,9 +20,9 @@ export default class ChromeBoisDomain extends Component {
    * `toggleCycling` function with no arguments. Don't forget the click event
    * listener that should fire it!
    */
-handleClick() {
-  toggleCycling();
-};
+  handleClick() {
+    toggleCycling();
+  };
 
   /* TODO: Add an event listener to the `<canvas>` element to capture when a key
   /* is pressed. When a key is pressed, an event handler should invoke the
@@ -30,6 +30,15 @@ handleClick() {
   /* if the key pressed was 'a', then it should call `resize` with '+'
   /* if the key pressed was 's', then it should call `resize` with '-'
    */
+
+   handleKeyDown(event) {
+     if (event.key === 'a') {
+      return resize('+')
+    } 
+    else if (event.key === 's') {
+      return resize('-')
+    };
+  };
 
   render() {
     return (
